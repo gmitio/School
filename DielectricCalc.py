ee243bab68ce32fd6e30a60c68d9d1e235306cf5
Graison Mitio (grm011) <grm011@engr-elec74-11L.usask.ca>
2020-01-29 10:49:03 -0600

    Updated Dielectric Calc

--- DielectricCalc.py
+++ DielectricCalc.py
@@ -2,14 +2,18 @@ import math
 
 bw = 70e-9	# Bandwidth of dielectric mirror
 wl = 1550e-9	# Central wavelength
-N = 10		# Number of stacks
-d1 = 0.2e-3	# Distance of layer 1
-d2 = 0.3e-3	# Distance of layer 2
-n1 = 2.33	# Refractive index of first layer
-n2 = 1.21	# Refractive index of second layer
-# n1*d1 + n2*d2 = wl/2
-R = math.pow( (math.pow(n1,2*N) - (n0/n3)*math.pow(n2, 2*N)) / ((math.pow(n1,2*N) - (n0/n3)*math.pow(n2, 2*N))) ,2)
-
+N = 35		# Number of stacks
+d1 = 6e-6	# Thickness of layer 1
+d2 = 5.881e-6	# Thickness of layer 2
+n0 = 1		# Refractive index of air
+n1 = 3.5243	# Refractive index of first layer
+n2 = 3.257	# Refractive index of second layer
+n3 = 1.9743	# Refractive index of substrate
+print( (n1*d1 + n2*d2) * 2)
+#d2 = (wl*52/2 - n1*d1)/n2
+#print(d2)
+R = math.pow( (math.pow(n1,2*N) - (n0/n3)*math.pow(n2, 2*N)) / ((math.pow(n1,2*N) + (n0/n3)*math.pow(n2, 2*N))) ,2)
+print(R)
 # bw/wl = (4/math.pi)*math.arcsin( (n1-n2) / (n1+n2) )
 # it follows that 
   # x = math.sin(math.pi*bw/(4*wl))
